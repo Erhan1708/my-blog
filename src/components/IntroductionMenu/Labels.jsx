73e035e5a91a79648fa24cb27a46baf757b953ad
Erhan <erhankubanych@gmail.com>
2022-07-26 09:21:34 +0600

    feat: added logic and functionality for title buttons

--- src/components/IntroductionMenu/Labels.jsx
+++ src/components/IntroductionMenu/Labels.jsx
@@ -4,13 +4,32 @@ import { tags } from "../../constants";
 const margin = { marginRight: "5px" };
 
 const Labels = () => {
-	const tagsArray =  tags.map(tag => (
-			<span
-				className="w3-tag w3-light-grey w3-small w3-margin-bottom"
-				style={margin}>
-				{tag}
-			</span>
-		));
+	// const tagsArray = tags.map((tag) => (
+	// 	<span
+	// 		className="w3-tag w3-light-grey w3-small w3-margin-bottom"
+	// 		style={margin}
+	// 	>
+	// 		{tag}
+	// 	</span>
+	// ));
+   const removeActive = () => {
+      const elements = document.querySelectorAll('.w3-tag')
+      
+      for (let i = 0; i < elements.length; i++) {
+         if (elements[i].classList.contains('w3-black')) {
+            elements[i].classList.remove('w3-black');
+            elements[i].classList.add("w3-light-grey");
+            elements[i].classList.add("w3-small");
+         }
+      }
+   }
+
+   const activated = (e) => {
+      removeActive();
+      e.target.classList.add('w3-black');
+      e.target.classList.remove('w3-light-grey');
+      e.target.classList.remove("w3-small");
+}
 
 	return (
 		<>
@@ -20,9 +39,16 @@ const Labels = () => {
 					<h4>Tags</h4>
 				</div>
 				<div className="w3-container w3-white">
-               <p>
-                  {tagsArray}
-               </p>
+					<p>
+						{tags.map(tag => (
+                     <span
+                        onClick={activated}
+								className="w3-tag w3-light-grey w3-small w3-margin-bottom"
+								style={margin}>
+								{tag}
+							</span>
+						))}
+					</p>
 				</div>
 			</div>
 		</>

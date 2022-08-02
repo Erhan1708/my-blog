90df94016c0280bc2f4209a3f8acbcf432670917
Erhan <erhankubanych@gmail.com>
2022-08-02 10:19:25 +0600

    delit: deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import BlockEntries from './components/BlockEntries';
-import Footer from './components/Footer'
-import Header from './components/Header';
-import IntroductionMenu from './components/IntroductionMenu/IntroductionMenu';
 
 const App = () => {
   return (
 		<>
-			<div className="w3-content" style={{ maxWidth: 1400 }}>
-				<Header />
-           <div className="w3-row">
-              <BlockEntries/>
-              <IntroductionMenu/>
-            </div>
-			</div>
-
-			<Footer />
+			
 		</>
 	);
 }
